20450a4a6a0558a2b1ca9ea05e6bcc596eff19e1
Sahil Khan <skhan@argusoft.com>
2023-09-21 18:18:15 +0530

    js started

---
+++ JavaScript/section4/app.js
@@ -0,0 +1,64 @@
+// if(true || console.log("two")){
+//     console.log("three");
+// }
+
+
+// let day =3;
+// switch(day){
+
+//     case 1: console.log("Mon");
+
+//     default:console.log("invalid");
+
+//     case 2: console.log("Tue");
+    
+//     case 1: console.log("Mon3");
+
+// }
+
+
+
+
+// let arr = [1];
+// arr[2]=2;
+// console.log(arr[1]);
+
+
+// let arr = [1,2,3,4];
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+
+// let arr = [1,2,3,4];
+// console.log(arr.splice(1,2,3,4));
+// console.log(arr);
+
+
+// let arr = [2,1,11,4];
+// arr.sort();
+// console.log(arr);
+
+
+// const numbers = [11,"gd",2,5,"abc",6,"xyz"];
+// // Sort numerically (ascending order)
+// numbers.sort((a, b) => a - b);
+// console.log(numbers); 
+
+
+// let arr1 = [1,2,3,4];
+// let arr2 = arr1;
+// arr1.push(5);
+// console.log(arr2);
+
+// if(NaN < 1){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// let a = "32";
+// console.log(!!+a);
