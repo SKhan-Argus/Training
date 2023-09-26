10dbcad753e995cf691f074a6ac0d6ff5913a16d
Sahil Khan <skhan@argusoft.com>
2023-09-26 19:07:23 +0530

    dom js

---
+++ JavaScript/dom/app.js
@@ -0,0 +1,5 @@
+// console.log(document.getElementById("bear-photo"));
+// console.log(document.getElementsByTagName("li"));
+
+const liCollection = document.getElementsByTagName("li");
+console.dir(liCollection[0]);
--- JavaScript/section4/app.js
+++ JavaScript/section4/app.js
@@ -2,7 +2,6 @@
 //     console.log("three");
 // }
 
-
 // let day =3;
 // switch(day){
 
@@ -11,18 +10,14 @@
 //     default:console.log("invalid");
 
 //     case 2: console.log("Tue");
-    
+
 //     case 1: console.log("Mon3");
 
 // }
 
-
-
-
 // let arr = [1];
 // arr[2]=2;
 // console.log(arr[1]);
-
 
 // let arr = [1,2,3,4];
 // console.log(arr);
@@ -31,22 +26,18 @@
 // arr.pop();
 // console.log(arr);
 
-
 // let arr = [1,2,3,4];
 // console.log(arr.splice(1,2,3,4));
 // console.log(arr);
-
 
 // let arr = [2,1,11,4];
 // arr.sort();
 // console.log(arr);
 
-
 // const numbers = [11,"gd",2,5,"abc",6,"xyz"];
 // // Sort numerically (ascending order)
 // numbers.sort((a, b) => a - b);
-// console.log(numbers); 
-
+// console.log(numbers);
 
 // let arr1 = [1,2,3,4];
 // let arr2 = arr1;
@@ -63,7 +54,6 @@
 // let a = "32";
 // console.log(!!+a);
 
-
 // if(console.log(console.log("one") | console.log("two"))){
 //     console.log("three");
 // }
@@ -75,13 +65,11 @@
 // display();
 // console.log(a);
 
-
 // var a = 10;
 
 // function display(){
 //   console.log("hello");
 // }
-
 
 // let account = {
 //   username: 'sahilkhan',
@@ -98,7 +86,6 @@
 //   guessedNumber = Math.floor(Math.random() * 10) + 1;
 //   console.log(guessedNumber);
 // }
-
 
 // function isValidPassword(password, username){
 //   if(password.length >= 8 && !(password.includes(username)) && !(password.includes(" "))){
@@ -132,7 +119,6 @@
 
 // console.log(checkPangram('the five boxing wizards jump quickly.'));
 
-
 // let cards = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
 // let suits = ['clubs', 'spades', 'hearts', 'diamonds'];
 
@@ -148,7 +134,6 @@
 // console.log(getCard());
 // console.log(getCard());
 // console.log(getCard());
-
 
 // function test(){
 //   let a= 10;
@@ -171,22 +156,17 @@
 
 // higher(display, 10);
 
-
-
 // function multiplyBy(num){
 //   return function(x){
 //     return num*x;
 //   }
 // }
 
-
-
 // const triple = multiplyBy(3);
 // console.log(triple(20));
 
 // const double = multiplyBy(2);
 // console.log(double(10));
-
 
 // let arr = [1,2,3,4,5];
 // arr.forEach(function(num){
@@ -231,6 +211,180 @@
 //     return num%2==1;
 // }));
 
+// let arr = [23,4,34,65,3];
+// console.log(arr.sort());
 
-let arr = [23,4,34,65,3];
-console.log(arr.sort());
+// const arr = [34,65,31,76,43,98];
+// const maxValue = arr.reduce((max,currValue) => {
+//     return currValue>max ? currValue : max;
+// });
+// console.log(maxValue);
+
+// const votes = ['y', 'y', 'n', 'y', 'n', 'n', 'y', 'n'];
+
+// const result = votes.reduce((tally, val)=>{
+//     if(tally[val]){
+//         tally[val]++;
+//     }
+//     else{
+//         tally[val]=1;
+//     }
+//     return tally
+// }, {});
+// console.log(result);
+
+// const nums  = [12,23,12,4,23,7,34];
+// console.log(Math.max(...nums));
+// console.log(...nums);
+
+// const obj1 = {a:1, b:2, c:3};
+// const obj2 = {...obj1, d:4};
+// console.log(obj2);
+
+// console.log({..."sahil khan"});
+
+// function display(a,b){
+//     console.log(arguments);
+// }
+// display(2,3);
+
+// function add(a,b,...nums){
+//     console.log(nums);
+// }
+// const numbers = [1,2,3,4,5];
+// add(...numbers);
+
+// const nums = [1,2,3,4,5,6];
+// const [a,b,c,...d] = nums;
+// console.log(d);
+
+// const library = {
+//     name: "My Library",
+//     location: "Cityville",
+//     books: [
+//       {
+//         title: "Book 1",
+//         author: {
+//           name: "Author 1",
+//           nationality: "Nationality 1",
+//         },
+//         genre: "Genre 1",
+//       },
+//       {
+//         title: "Book 2",
+//         author: {
+//           name: "Author 2",
+//           nationality: "Nationality 2",
+//         },
+//         genre: "Genre 2",
+//       },
+//       {
+//         title: "Book 3",
+//         author: {
+//           name: "Author 1",
+//           nationality: "Nationality 1",
+//         },
+//         genre: "Genre 1",
+//       },
+//     ],
+//   };
+
+//   console.log(library);
+
+// function display({name:libraryName,books:[{title:title1},{title:title2},{title:title3}]}){
+//     console.log(libraryName);
+//     console.log(title1, title2, title3);
+// }
+
+// display(library)
+
+// const add = function(a,b){
+//     return a+b;
+// }
+
+// const mul = function(a,b){
+//     return a*b;
+// }
+
+// const math = {
+//     add,
+//     mul,
+//     nums:[1,2],
+// }
+// console.log(math.add(...math['nums']));
+
+// console.log(this);
+
+// function display(){
+//     console.log(this);
+// }
+
+// const obj1 = {
+//     firstName:'sahil',
+//     lastName:'khan',
+//     show(){
+//         console.log(this);
+//     }
+// }
+
+// const show = obj1.show;
+// show();
+
+// const test = {
+//     name: 'sahil',
+//     print(){
+//         console.log(this.name);
+//     },
+//     start(){
+//         setTimeout(()=>{
+//             this.print();
+//         }, 2000);
+//     }
+// };
+// test.start();
+
+// ----------------------------------------
+// deck game
+const myDeck = {
+  deck: [],
+  suits: ["spades", "hearts", "diamonds", "clubs"],
+  values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+  player1Drawn: [],
+  player2Drawn: [],
+  intializeDeck: function () {
+    for (let suit of this.suits) {
+      for (let value of this.values.split(",")) {
+        this.deck.push({
+          value,
+          suit,
+        });
+      }
+    }
+  },
+  shuffleDeck: function () {
+    for (let i = 0; i < this.deck.length; i++) {
+      let index = Math.floor(Math.random() * 52);
+      [this.deck[i], this.deck[index]] = [this.deck[index], this.deck[i]];
+    }
+  },
+  drawPlayer1Card: function () {
+    this.player1Drawn.push(this.deck.pop());
+  },
+  drawPlayer2Card: function () {
+    this.player2Drawn.push(this.deck.pop());
+  },
+  startGame: function () {
+    this.intializeDeck();
+    this.shuffleDeck();
+    this.drawPlayer1Card();
+    this.drawPlayer2Card();
+    this.drawPlayer1Card();
+    this.drawPlayer2Card();
+    this.drawPlayer1Card();
+    this.drawPlayer2Card();
+    console.log(this.player1Drawn);
+    console.log(this.player2Drawn);
+  },
+};
+
+myDeck.startGame();
