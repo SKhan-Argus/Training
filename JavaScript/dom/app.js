c73d380ae98ed549f717c99413607d70c2a2db3c
Sahil Khan <skhan@argusoft.com>
2023-09-27 16:49:48 +0530

    dom project

--- JavaScript/dom/app.js
+++ JavaScript/dom/app.js
@@ -1,5 +1,35 @@
 // console.log(document.getElementById("bear-photo"));
 // console.log(document.getElementsByTagName("li"));
 
-const liCollection = document.getElementsByTagName("li");
-console.dir(liCollection[0]);
+// const liCollection = document.getElementsByTagName("li");
+// console.dir(liCollection[0]);
+
+// const liItems = document.getElementsByTagName("li");
+// const liItems2 = document.querySelectorAll("li");
+// liItems2[1].remove();
+// console.log(liItems);
+// console.log(liItems2);
+
+// const ul = document.querySelector("ul");
+// console.log(ul.innerHTML);
+
+// const range = document.querySelector("input[type='range']");
+// console.log(range.value);
+
+// const el = document.querySelector("ul");
+// console.log(el);
+// console.log(el.children);
+// console.log(el.parentElement);
+// console.log(el.children[0].nextElementSibling);
+
+// const h1 = document.querySelector("h1");
+// h1.style.fontSize = "150px";
+// console.log(h1.style.fontSize);
+
+// const h1 = document.querySelector("h1");
+// console.log(getComputedStyle(h1).fontFamily);
+
+// const todo = document.querySelectorAll("#todos .todo");
+// console.log(todo);
+
+// todo[0].classList.add("done");
---
+++ JavaScript/dom_project/app.js
@@ -0,0 +1,104 @@
+const warriorsGames = [
+  {
+    awayTeam: {
+      team: "Golden State",
+      points: 119,
+      isWinner: true,
+    },
+    homeTeam: {
+      team: "Houston",
+      points: 106,
+      isWinner: false,
+    },
+  },
+  {
+    awayTeam: {
+      team: "Golden State",
+      points: 105,
+      isWinner: false,
+    },
+    homeTeam: {
+      team: "Houston",
+      points: 127,
+      isWinner: true,
+    },
+  },
+  {
+    homeTeam: {
+      team: "Golden State",
+      points: 126,
+      isWinner: true,
+    },
+    awayTeam: {
+      team: "Houston",
+      points: 85,
+      isWinner: false,
+    },
+  },
+  {
+    homeTeam: {
+      team: "Golden State",
+      points: 92,
+      isWinner: false,
+    },
+    awayTeam: {
+      team: "Houston",
+      points: 95,
+      isWinner: true,
+    },
+  },
+  {
+    awayTeam: {
+      team: "Golden State",
+      points: 94,
+      isWinner: false,
+    },
+    homeTeam: {
+      team: "Houston",
+      points: 98,
+      isWinner: true,
+    },
+  },
+  {
+    homeTeam: {
+      team: "Golden State",
+      points: 115,
+      isWinner: true,
+    },
+    awayTeam: {
+      team: "Houston",
+      points: 86,
+      isWinner: false,
+    },
+  },
+  {
+    awayTeam: {
+      team: "Golden State",
+      points: 101,
+      isWinner: true,
+    },
+    homeTeam: {
+      team: "Houston",
+      points: 92,
+      isWinner: false,
+    },
+  },
+];
+
+const ul = document.querySelector("#list");
+
+for (let match of warriorsGames) {
+  let item = document.createElement("li");
+  item.innerHTML = `${match.awayTeam.team} @ ${
+    match.homeTeam.team
+  } <span class=${match.awayTeam.isWinner ? "win-score" : ""}>${
+    match.awayTeam.points
+  }</span> - <span class=${match.homeTeam.isWinner ? "win-score" : ""}>${
+    match.homeTeam.points
+  }</span>`;
+
+  const warriors =
+    match.awayTeam.team === "Golden State" ? match.awayTeam : match.homeTeam;
+  item.classList.add(warriors.isWinner ? "win" : "lose");
+  ul.appendChild(item);
+}
