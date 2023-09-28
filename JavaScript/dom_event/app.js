5d1e76eeb9f24729f27cd9391759efd57ee0f03f
Sahil Khan <skhan@argusoft.com>
2023-09-28 18:26:11 +0530

    event listener

---
+++ JavaScript/dom_event/app.js
@@ -0,0 +1,28 @@
+// const button = document.querySelector("#click");
+
+// button.addEventListener("mouseover", () => {
+//   button.innerText = "Move Away";
+// });
+
+// button.addEventListener("mouseout", () => {
+//   button.innerText = "Click Me!";
+// });
+
+// window.addEventListener("wheel", () => {
+//   console.log("mouse wheel");
+// });
+
+// const button2 = document.querySelector("#impossible");
+// console.log(button2);
+// button2.addEventListener("mouseover", () => {
+//   const left1 = Math.floor(Math.random() * 500) + 50;
+//   const top1 = Math.floor(Math.random() * 500) + 50;
+//   console.log(left1, top1);
+//   button2.style.left = `${left1}px`;
+//   button2.style.top = `${top1}px`;
+// });
+
+// const li = document.getElementsByTagName("li");
+// const li2 = document.querySelectorAll("li");
+// console.log(li);
+// console.log(li2);
